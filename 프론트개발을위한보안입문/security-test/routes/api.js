40edc144dadba303f34fa8f604c062574069326c
윤서림 <tree@hopae.io>
2024-02-17 03:45:48 +0900

    fix  change query name

--- 프론트개발을위한보안입문/security-test/routes/api.js
+++ 프론트개발을위한보안입문/security-test/routes/api.js
@@ -2,10 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ message: "Hello" });
+  res.setHeader("X-Timestamp", Date.now());
+
+  console.log(req.query);
+  let message = req.query.message;
+  const lang = req.headers["x-lang"];
+
+  console.log(message);
+
+  if (message === "") {
+    res.status(400);
+    if (lang === "en") {
+      message = "message is empty";
+    } else {
+      message = "message 값이 비었음.";
+    }
+    message = "message값이 비었어요.";
+  }
+  res.send({ message });
 });
-// router.post("/", (req, res) => {
-//   const body = req.body;
+
+// const res = await fetch("http://localhost:3000/api?message=", {
+//   headers: { "X-Lang": "en" },
 // });
+
+router.post("/", (req, res) => {
+  const body = req.body;
+});
 
 module.exports = router;
