1bd87f34a2b6e3c7e9dd82b16c34e022331ebb03
tree <hhyjs2r@gmail.com>
2024-03-02 14:01:32 +0900

    add strict-dynamic testing

--- 프론트개발을위한보안입문/security-test/ejsServer.js
+++ 프론트개발을위한보안입문/security-test/ejsServer.js
@@ -2,14 +2,20 @@ const express = require("express");
 const crypto = require("crypto");
 const app = express();
 const port = 3000;
+const path = require("path");
 
 app.set("view engine", "ejs");
 // Express는 기본적으로 "views"라는 폴더를 템플릿 파일의 기본 디렉토리로 간주한다.
 
+app.use(express.static(path.join(__dirname, "public")));
+// Serve static files from the 'public' directory
+
 app.get("/csp", (req, res) => {
   const nonceValue = crypto.randomBytes(16).toString("base64");
-  res.header("Content-Security-Policy", `script-src 'nonce-${nonceValue}'`);
-  // script 가 비활성화된 상태이므로 인라인 스크립트가 실행되지 않는다.
+  res.header(
+    "Content-Security-Policy",
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic'`
+  );
 
   res.render("csp", { nonce: nonceValue });
 });
---
+++ 프론트개발을위한보안입문/security-test/public/csp-test.js
@@ -0,0 +1,1 @@
+alert("csp-test.js의 스크릡트가 실행됨");
