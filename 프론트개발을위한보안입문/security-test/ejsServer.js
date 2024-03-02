4925834f84786247d7304de2931938cb29c8ec53
tree <hhyjs2r@gmail.com>
2024-03-02 13:18:17 +0900

    insert nonce in header

--- 프론트개발을위한보안입문/security-test/ejsServer.js
+++ 프론트개발을위한보안입문/security-test/ejsServer.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const crypto = require("crypto");
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-// Express는 기본적으로 "views"라는 폴더를 템플릿 파일의 기본 디렉토리로 간주힌다.
+// Express는 기본적으로 "views"라는 폴더를 템플릿 파일의 기본 디렉토리로 간주한다.
 
 app.get("/csp", (req, res) => {
-  res.header("Content-Security-Policy", "script-src 'self'");
-  res.render("csp");
+  const nonceValue = crypto.randomBytes(16).toString("base64");
+  res.header("Content-Security-Policy", `script-src 'nonce-${nonceValue}'`);
+  // script 가 비활성화된 상태이므로 인라인 스크립트가 실행되지 않는다.
+
+  res.render("csp", { nonce: nonceValue });
 });
 
 app.listen(port, () => {
