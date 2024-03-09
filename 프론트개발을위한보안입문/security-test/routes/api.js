c9d9cbb0e47545a3ba7e63b614347b0813955246
tree <hhyjs2r@gmail.com>
2024-03-09 12:51:08 +0900

    add cors header list

--- 프론트개발을위한보안입문/security-test/routes/api.js
+++ 프론트개발을위한보안입문/security-test/routes/api.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const allowList = ["http://localhost:3000", "http://site.example:3000"];
+
+router.use((req, res, next) => {
+  const headerOrigim = req.headers.origin;
+  if (headerOrigim && allowList.includes(headerOrigim))
+    //요청을 허가하는 리스트 내 Origin 헤더값이 포함됐는지 체크
+    res.header("Acess-Control-Allow_origin", headerOrigim);
+
+  if (req.method === "OPTIONS") {
+    res.header("Acess-Control-Allow-Headers", "X-Token");
+  }
+  next();
+});
+
 router.get("/", (req, res) => {
   res.setHeader("X-Timestamp", Date.now());
 
@@ -19,12 +33,23 @@ router.get("/", (req, res) => {
   res.send({ message });
 });
 
-// const res = await fetch("http://localhost:3000/api?message=", {
-//   headers: { "X-Lang": "en" },
-// });
+router.get("/test", (req, res) => {
+  res.send({ a: "asdffsdffdsafdfda" });
+});
 
 router.post("/", (req, res) => {
   const body = req.body;
 });
 
 module.exports = router;
+
+// 70p ~
+
+// const res = await fetch("http://localhost:3000/api?message=", {
+//   headers: { "X-Lang": "en" },
+// });
+
+//해당 요청을 보냈을때 404 에러가 뜬다.
+//api 요청에 접근할 수 있는 이유는 server.js에서 미들웨어에 api 경로를 설정해주었기 때문이다.
+
+//test경로를 설정해줌으로써 api/test로 api를 호출할 수 있다.
