6920a3a3dabec4f0a7d3898493bcc9b7acea3abf
tree <hhyjs2r@gmail.com>
2024-08-17 20:14:40 +0900

    study: http 네트워크 레이어,http프로토콜

---
+++ 알고리즘/03.정렬.js
@@ -0,0 +1,56 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+let array = [5, 3, 8, 4, 2];
+console.log("Sorted array:", bubbleSort(array));
+
+function selectionSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+let array2 = [29, 10, 14, 37, 14];
+console.log("Sorted array:", selectionSort(array));
+function insertionSort(arr) {
+  let n = arr.length;
+  for (let i = 1; i < n; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+let array3 = [4, 3, 2, 10, 12, 1, 5, 6];
+console.log("Sorted array:", insertionSort(array));
